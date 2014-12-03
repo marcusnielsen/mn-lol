dcddb3c5f7f2d536fcfcb4051cf1b301d5f99d3a
Marcus Nielsen <marcus.nielsen82@gmail.com>
2014-12-03 22:40:50 +0100

    Refactored structure to work with npm and heroku.

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ var app = express()
 var path = require('path')
 
 app.use(livereload())
-app.use(express.static(path.join(__dirname, '../task-builder/dist')))
+app.use(express.static(path.join(__dirname, '../client')))
 
 var port = process.env.PORT || 3000
 console.log('Server started on port ' + port + '.')
